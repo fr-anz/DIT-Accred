1802253f5ca5a7d8943dfc498dffbff8abbb4f00
fr-anz <franzemmanuelbaes@gmail.com>
2026-05-22 18:26:38 +0800

    chore: integrated current navbar to storybook for preview

--- src/stories/Navbar.stories.svelte
+++ src/stories/Navbar.stories.svelte
@@ -12,16 +12,8 @@
 			docs: {
 				story: { inline: false, iframeHeight: 500 }
 			}
-		},
-		argTypes: {
-			currentPath: {
-				control: { type: 'select' },
-				options: ['/', '/area', '/report', '/certificate', '/accreditation']
-			}
 		}
 	});
 </script>
 
-<Story name="Default" args={{ currentPath: '/' }} />
-<Story name="Active: Level 3 Phase 2 Area" args={{ currentPath: '/area' }} />
-<Story name="Active: Compliance Report" args={{ currentPath: '/report' }} />
+<Story name="Default" />
